662174722ac7e9be6bda626ae1f1a30847072295
NataliaWszelaki <126349718+NataliaWszelaki@users.noreply.github.com>
2024-01-27 23:42:09 +0100

    first commit

---
+++ src/main/java/com/crud/adventuretravel/AdventureTravelApiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.crud.adventuretravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureTravelApiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdventureTravelApiBackendApplication.class, args);
+    }
+
+}
